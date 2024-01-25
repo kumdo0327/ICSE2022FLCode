214ce6d7823bf6cb8ee03b6f19f132c1351fd43a
kumdo0327 <kumdo0327@naver.com>
2024-01-25 20:37:59 +0900

    update

--- pipeline/Pipeline.py
+++ pipeline/Pipeline.py
@@ -61,7 +61,9 @@ class Pipeline:
         elif self.experiment == "fs":
             cp = float(self.configs["-cp"])
             ep = float(self.configs["-ep"])
+            print('Pipeline.py : self.data_obj = PCAData(self.dataloader, "/volume/aeneas/cache", os.path.join(self.project_dir, "time/io"))')
             self.data_obj = PCAData(self.dataloader, "/volume/aeneas/cache", os.path.join(self.project_dir, "time/io"))
+            print('\tdone')
             return
             #self.data_obj.process(cp, ep)
         elif self.experiment == "fs_cvae":
