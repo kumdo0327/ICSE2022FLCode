50b86e0d025ba5bae191ab23a73b833a5af740b9
kumdo0327 <kumdo0327@naver.com>
2024-01-30 13:49:12 +0900

    update

--- pipeline/Pipeline.py
+++ pipeline/Pipeline.py
@@ -78,7 +78,7 @@ class Pipeline:
 
         for m in self.method:
             print('CalculateSuspiciousness', m)
-            save_rank_path = os.path.join(self.project_dir, f"results/e2e/{self.experiment}/{m}/{self.program}")
+            save_rank_path = os.path.join(self.project_dir, f"results/{self.experiment}/{m}/{self.program}")
             if not os.path.exists(save_rank_path):
                 os.makedirs(save_rank_path)
             cc = CalculateSuspiciousness(self.data_obj, [m], save_rank_path, self.experiment, int(time.time() - self.start))
